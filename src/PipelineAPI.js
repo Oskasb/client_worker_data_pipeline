aa0ee30caae783f3bc1d829c73060d6f9e9bd4be
oskar.asbrink@gmail.com <oskar.asbrink@gmail.com>
2014-10-15 14:17:41 +0200

    add pipeline api

---
+++ src/PipelineAPI.js
@@ -0,0 +1,42 @@
+"use strict";
+
+define(['data_pipeline/data/ConfigCache'],
+	function(
+		ConfigCache
+		) {
+
+		var PipelineAPI = function() {
+
+		};
+
+		PipelineAPI.readCachedConfigKey = function(category, key) {
+			ConfigCache.getConfigKey(category, key)
+		};
+
+		PipelineAPI.subscribeToCategoryUpdate = function(category, onDataCallback) {
+			ConfigCache.registerCategoryUpdatedCallback(category, onDataCallback)
+		};
+
+		PipelineAPI.subscribeToCategoryKey = function(category, key, onDataCallback) {
+			ConfigCache.subscribeToCategoryKey(category, key, onDataCallback)
+		};
+
+		PipelineAPI.subscribeToConfigUrl = function(url, success, fail) {
+			ConfigCache.cacheFromUrl(url, success, fail)
+		};
+
+		PipelineAPI.cacheSvgFromUrl = function(url, success, fail) {
+			ConfigCache.cacheSvgFromUrl(url, success, fail)
+		};
+
+		PipelineAPI.getCachedConfigs = function() {
+			return ConfigCache.getCachedConfigs();
+		};
+
+		PipelineAPI.updateDataPipeline = function(tpf) {
+			ConfigCache.tickDataPipeline(tpf);
+		};
+
+
+		return PipelineAPI;
+	});
